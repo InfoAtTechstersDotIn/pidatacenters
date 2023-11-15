41547fecc46a24f8150c6dbf7984ef143f31cb16
shruthi <shruthi.y@techsters.in>
2023-11-15 12:06:13 +0530

    checking

--- dc-build.php
+++ dc-build.php
@@ -9,7 +9,7 @@ $seo_keywords = "Data center build, data center technology, network security, IT
 
 <div class="about-banner">
     <div class="about-banner-blk">
-        <img src="images/dc-build-banner.jpg" alt="dc-build-banner" width="1920" height="210">
+        <img src="images/dc-build-banner.jpg" alt="dc-build-banner" width="1920" height="210" class="datacenter-services-img">
         <h2>Data Center Build & operate</h2>
 
     </div>
