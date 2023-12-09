2105306c0bd3cf58e11091ca8d4fa0c72b7a240a
shruthi <shruthi.y@techsters.in>
2023-12-09 11:49:22 +0530

    checking

---
+++ new-noc.php
@@ -0,0 +1,402 @@
+<?php
+$seo_title = "Network Operating Center - Pi Datacenters | World-Class Network Troubleshooting";
+$seo_description = "Manage Complex Network Infrastructure with a World-Class Network Operating Center at Pi Datacenters That Continuously Supervises, Monitors & Maintains Your Networks & Manages Network Troubleshooting & Software Distribution to Minimize Disruptions & Downtime. ";
+$seo_keywords = "Network Operating Center, network operations center,  network troubleshooting, software distribution, complex network infrastructure, Patch management software";
+?>
+
+
+<?php include('php/includes-techsters/header.php'); ?>
+
+<!-- main starts here -->
+
+<div class="about-banner">
+    <div class="about-banner-blk">
+        <img src="images/noc-banner.jpg" alt="noc-banner" width="1920" height="210">
+        <h2>NOC</h2>
+
+    </div>
+</div>
+
+<style>
+    .noc {
+        max-width: 90%;
+        margin: 0 auto;
+    }
+
+    .noc h2 {
+        color: #000;
+        text-align: center;
+        margin: 20px 0;
+    }
+
+    .noc p {
+        color: #000;
+        text-align: justify;
+    }
+
+    .noc h4 {
+        color: #000;
+        text-align: center;
+        font-weight: 500;
+        /* padding: 20px 0 10px 0; */
+        font-size: 18px;
+    }
+
+    .bullets {
+        padding-left: 2rem !important;
+    }
+
+
+    .noc-part-2 .col-md-6 img {
+        border-top-right-radius: 50px;
+        border-bottom-left-radius: 50px;
+    }
+
+    .bullets li {
+        padding: 6px 0;
+        font-size: 18px;
+        margin: 10px 0;
+        border: 1px solid #cccccc91;
+        text-align: center;
+        background-color: #76b54330;
+        border-radius: 20px;
+    }
+
+    .bullets li:hover {
+        box-shadow: 1px 6px #223f97;
+        transform: translate(-6px, -5px);
+    }
+
+    .noc-part img {
+        border-radius: 15px;
+    }
+
+    .noc-part-4 h5 {
+        font-weight: 900;
+        text-align: center;
+        font-size: 15px;
+        padding: 5px 0;
+    }
+
+    .noc-part-4 .col-md-3 p {
+        text-align: center;
+        font-size: 14px;
+    }
+
+    .noc-part-4 .col-md-3 {
+        background: #cccccc4d;
+        border: 6px solid #223f97;
+        display: flex;
+        align-items: center;
+        padding: 10px 3px 10px 3px;
+        flex-direction: column;
+    }
+
+    .noc-part-3 img {
+        border-radius: 20px;
+    }
+
+
+    .noc-part-5 .col-md-3,
+    .noc-part-5 .col-md-4 {
+        display: inline-block;
+        vertical-align: middle;
+        margin: 10px auto;
+        background: #f1f1f100;
+        border-radius: 20px;
+        padding: 1rem;
+        box-shadow: 0px 1px 7px black;
+    }
+
+
+
+    .noc-part-5 .col-md-3 img {
+        width: 30%;
+        margin: 0 auto 5px auto;
+    }
+
+    .noc-part-5 .col-md-4 img {
+        width: 20%;
+        margin: 0 auto 5px auto;
+    }
+
+    .noc-part-5 h5 {
+        text-align: center;
+        font-weight: 600;
+        font-size: 20px;
+        padding: 5px 0;
+    }
+
+    .noc-part-5 .col-md-3 p,
+    .noc-part-5 .col-md-4 p {
+        text-align: center;
+    }
+
+    .noc-part-5 .last-para p {
+        text-align: center;
+        margin: 10px;
+    }
+
+    .hide-cont {
+        display: none;
+        position: absolute;
+        top: 0;
+        background-color: #223f97cc;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        padding: 60px 15px;
+        text-align: center;
+        border-radius: 20px;
+    }
+
+    .hide-cont p {
+        color: #fff;
+        font-weight: 600;
+    }
+
+    .noc-part-5 .col-md-3:hover .hide-cont,
+    .noc-part-5 .col-md-4:hover .hide-cont {
+        display: block;
+    }
+
+    .noc-part-2 img {
+        width: 50%;
+        margin: 0 auto;
+        border-radius: 20px;
+    }
+
+    .single-point {
+        margin: 0 auto;
+        border-radius: 20px;
+    }
+</style>
+<!--what-is-noc starts here -->
+<div class="noc-block">
+    <div class="noc">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Why <span style="color: #76b543;">NOC</span>?</h2>
+                    <p>NOC services are essential to any modern business's IT infrastructure. As a service, Pi's Network Operating Center (NOC) aims to ensure that your business's networks are continuously supervised, monitored, and maintained to minimize disruptions and downtime. It is designed to help organizations strengthen their networks' optimal performance and perform other business-critical tasks like network troubleshooting, software distribution & updating, coordination with interconnected networks, and more. </p>
+                    <h4>Our team of highly trained NOC engineers is available around the clock and carries out activities like: </h4>
+                </div>
+            </div>
+
+
+            <div class="noc-part-2">
+                <div class="row mt-4">
+                    <div class="col-md-12 mt"><img src="images/new-noc-1.png"></div>
+                    <div class="col-md-6 bullets mt-3">
+                        <ul>
+                            <li>Network Monitoring and Updates </li>
+                            <li>Communications Management </li>
+                            <li>Firewall Management </li>
+                            <li>Performance and Quality Reporting </li>
+                            <li>Security Alerts </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 bullets mt-3">
+                        <ul>
+                            <li>Threat Analysis </li>
+                            <li>Intrusion Prevention Systems </li>
+                            <li>Incident Response </li>
+                            <li>Policy Enforcement (As Per User Guidelines) </li>
+                            <li>Quality Control and Assurance (in terms of the NOC) </li>
+                        </ul>
+
+                    </div>
+                    <div class="col-md-6 bullets single-point">
+                        <ul>
+                            <li>Data and Storage Compliance </li>
+                        </ul>
+
+                    </div>
+                    <div class="pt-3">
+                        <p>The key benefit of our NOC service is real-time responses and quick resolution. A prompt response reduces disruptions and prevents major business losses. NOC as a service lets businesses analyze complex network infrastructure, identify disruption causes, and prevent future incidents. A proactive approach can protect sensitive data, avoid costly downtime, and ensure smooth operations. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="noc-part-3">
+                <h2>Why <span style="color: #76b543;">NOC @ Pi</span>?</h2>
+                <div class="row">
+                    <div class="col-md-5">
+                        <img src="images/new-noc-2.png">
+                    </div>
+                    <div class="col-md-7 py-5">
+                        <p>Our Network Operations Center is not just a reactionary force; it's a proactive ally in optimizing your network's performance and ensuring its security. This extends beyond the scope of simply monitoring for potential issues; it also encompasses implementing best practices to ensure that the network operates optimally and furnishing regular updates and reports on its status. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="noc-part-4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2>Advantages of <span style="color: #76b543;">Pi NOC</span>?</h2>
+                            <p>Our Network Operations Center is not just a reactionary force; it's a proactive ally in optimizing your network's performance and ensuring its security. This extends beyond the scope of simply monitoring for potential issues; it also encompasses implementing best practices to ensure that the network operates optimally and furnishing regular updates and reports on its status. </p>
+                        </div>
+
+                        <div class="row mt-3">
+                            <div class="col-md-3">
+                                <h5>Installing, Troubleshooting, and Patching</h5>
+                                <p>Our dedicated NOC team handles critical network software, ensuring seamless operations. </p>
+                            </div>
+                            <div class="col-md-3">
+                                <h5>Security Alerts and Reporting </h5>
+                                <p>Collaborating with your security team, we provide alerts and detailed reports to prevent potential incidents.</p>
+
+                            </div>
+                            <div class="col-md-3">
+                                <h5>Firewall Management</h5>
+                                <p>Monitor and manage firewalls to fortify ongoing network and business security.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <h5>Threat Identification and Mitigation </h5>
+                                <p> Adept at spotting and mitigating security issues, minimizing potential damage.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="noc-part-5">
+                <h2><span style="color: #76b543;">Pi</span> Value Proposition</h2>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/continued-monitoring-and-maintenance.png">
+                                <h5>Continued Monitoring and Maintenance</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p> Ensuring your network operates at its peak performance. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/zero-disruptions-and-downtime.png">
+                                <h5>Zero Disruptions and Downtime</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p>Minimizing interruptions for uninterrupted business operations. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/availability-24-7.png">
+                                <h5>24/7 Availability</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p>Our NOC resources are at your service round the clock. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/trained-noc-resources.png">
+                                <h5>Trained NOC Resources</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p>A team of highly skilled professionals dedicated to safeguarding your network. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/real-time-response-to-incidents.png">
+                                <h5>Real-time Response to Incidents</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p>Swift action to mitigate the impact of disruptions. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/prevent-issues.png">
+                                <h5>Prevent Issues</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p>Proactive measures to avoid severe business losses. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/identification-and-resolution-of-the-root-cause.png">
+                                <h5>Identification and Resolution of the Root Cause</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p> Analyzing disruptions at their source for long-term solutions. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/ongoing-support.png">
+                                <h5>Ongoing Support</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p>Beyond monitoring, we provide continuous support for your network. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/network-optimization.png">
+                                <h5>Network Optimization</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p> Implementing best practices to ensure your network operates optimally. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/regular-updates-and-reports.png">
+                                <h5>Regular Updates and Reports</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p>Furnishing detailed insights to keep you informed. </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="visible-cont">
+                                <img src="images/noc-icons/dedicated-service-and-support.png">
+                                <h5>Dedicated Service and Support</h5>
+                            </div>
+                            <div class="hide-cont">
+                                <p> Tailored support to meet your specific business needs. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="last-para">
+                        <p>Choose Pi for a partnership that goes beyond mere surveillance, offering proactive support and optimization to propel your business forward. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include('php/includes-techsters/footer.php'); ?>
+
+
+
+
+<!--footer ends-->
+<!-- wrapper ends -->
+<script src="js/wow.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://kenwheeler.github.io/slick/slick/slick.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/main-pi.js"></script>
+
+</body>
+
+</html>
